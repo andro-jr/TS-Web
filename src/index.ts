f60759191c39e79b37a0576d859dfe0c1f3c2b53
andro-jr <prabin20panta@gmail.com>
2024-05-17 14:03:58 +0545

    events creation and trigger

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Prabin", age: 44 });
 
-console.log(user.get("age"));
+user.on("change", () => {
+  console.log("changing");
+});
+user.on("save", () => {
+  console.log("saving");
+});
 
-user.set({ age: 55 });
-
-console.log(user.get("age"));
+user.trigger("change");
+user.trigger("save");

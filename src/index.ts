6c019e5fc3489055ade7f1ca76bafa3fefc54ee3
andro-jr <prabin20panta@gmail.com>
2024-05-17 19:27:10 +0545

    index cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-import { User } from "./models/User";
 
-const user = new User({ name: "Prabin", age: 44 });
 
-user.on("change", () => {
-  console.log("changing");
-});
-user.on("save", () => {
-  console.log("saving");
-});
 
-user.trigger("change");
-user.trigger("save");

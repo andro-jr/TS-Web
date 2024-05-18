7bec3369375d03a99de76dd359b30da3e024774c
andro-jr <prabin20panta@gmail.com>
2024-05-18 15:32:00 -0400

    saving user data

--- src/models/User.ts
+++ src/models/User.ts
@@ -44,7 +44,25 @@ export class User {
       .get(`http://localhost:3000/users/${this.get("id")}`)
       .then((response: AxiosResponse): void => {
         this.set(response.data);
-        console.log("response.data :", this.get("id"));
       });
+  }
+
+  save(): void {
+    const id = this.get("id");
+
+    if (id) {
+      axios
+        .put(`http://localhost:3000/users/${id}`, this.data)
+        .then((response: AxiosResponse): void => {
+          this.set(response.data);
+        });
+    } else {
+      axios
+        .post(`http://localhost:3000/users`, this.data)
+        .then((response: AxiosResponse): void => {
+          this.set(response.data);
+          console.log("response.data :", this.get("id"));
+        });
+    }
   }
 }

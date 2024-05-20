08502e93bbb28b68bcea1c4c3053f54469c1c1b7
andro-jr <prabin20panta@gmail.com>
2024-05-20 12:28:12 +0545

    set data and trigger

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "Prabin" });
+
+user.on("change", () => {
+  console.log("User was changed!");
+});
+
+user.set({ name: "New Name" });

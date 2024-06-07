ba410a9dc4735d7980460599cf525ddf8f73cab3
andro-jr <prabin20panta@gmail.com>
2024-06-07 13:49:53 +0545

    final user refactor

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,6 @@
+import { ApiSync } from "./ApiSync";
+import { Attributes } from "./Attributes";
+import { Eventing } from "./Eventing";
 import { Model } from "./Model";
 
 export interface UserProps {
@@ -8,4 +11,12 @@ export interface UserProps {
 
 const rootUrl = "http://localhost:3000";
 
-export class User extends Model<UserProps> {}
+export class User extends Model<UserProps> {
+  static buildClass(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
+  }
+}

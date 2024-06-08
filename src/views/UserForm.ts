e3b70f01955c30f7f996fc620ce3e4f6d730682e
andro-jr <prabin20panta@gmail.com>
2024-06-08 13:52:49 +0545

    update user and rerendering

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,7 +1,15 @@
 import { User } from "../models/User";
 
 export class UserForm {
-  constructor(public parent: Element, public model: User) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on("change", () => {
+      this.render();
+    });
+  }
 
   get template(): string {
     return `
@@ -10,19 +18,20 @@ export class UserForm {
                 <div>User Name: ${this.model.get("name")}</div>
                 <div>User Age: ${this.model.get("age")}</div>
                 <input />
+                <button class="set-age">Set Random Age</button>
                 <button>Submit</button>
             </div>`;
   }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      "click:button": this.onButtonClick,
+      "click:.set-age": this.onSetAgeClick,
     };
   }
 
-  onButtonClick(): void {
-    console.log("Hi there");
-  }
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
   bindEvents(fragment: DocumentFragment): void {
     const eventsMap = this.eventsMap();
@@ -37,6 +46,8 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = "";
+
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template;
 

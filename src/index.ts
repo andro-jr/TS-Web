9e72a5c673f696cadba9ba83efbddfbcce526f32
andro-jr <prabin20panta@gmail.com>
2024-06-08 11:21:18 +0545

    rendering html

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-import { User } from "./models/User";
+import { UserForm } from "./views/UserForm";
 
-const collection = User.buildUserCollection();
+const userForm = new UserForm(document.getElementById("root")!);
 
-collection.fetch();
-collection.on("change", () => {
-  console.log("Changed!!", collection);
-});
+userForm.render();

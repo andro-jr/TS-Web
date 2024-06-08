d4f0f2a569159044896a153429347418ec8ff4af
andro-jr <prabin20panta@gmail.com>
2024-06-08 13:59:41 +0545

    update user name

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -18,6 +18,7 @@ export class UserForm {
                 <div>User Name: ${this.model.get("name")}</div>
                 <div>User Age: ${this.model.get("age")}</div>
                 <input />
+                <button class="set-name">Change Name</button>
                 <button class="set-age">Set Random Age</button>
                 <button>Submit</button>
             </div>`;
@@ -26,11 +27,18 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       "click:.set-age": this.onSetAgeClick,
+      "click:.set-name": this.onSetNameClick,
     };
   }
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  };
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector("input")!;
+    const name = input.value;
+
+    this.model.set({ name });
   };
 
   bindEvents(fragment: DocumentFragment): void {
